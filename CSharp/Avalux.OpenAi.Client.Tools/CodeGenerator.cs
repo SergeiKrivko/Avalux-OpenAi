f99c34fab0132499a753bfbd9e4d2aac51df6f71
SergeiKrivko <sergik.krivko@gmail.com>
2025-06-17 21:34:15 +0300

    Prompt files

--- CSharp/Avalux.OpenAi.Client.Tools/CodeGenerator.cs
+++ CSharp/Avalux.OpenAi.Client.Tools/CodeGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Humanizer;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -23,7 +24,8 @@ namespace Avalux.OpenAi.Client.Tools
                 .AddUsings(
                     SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Net.Http.Json")),
                     SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Text.Json.Serialization")),
-                    SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Text.Json"))
+                    SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Text.Json")),
+                    SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("Avalux.OpenAi.Client"))
                 );
 
             var namespaceDeclaration = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName(codeNamespace))
@@ -130,19 +132,19 @@ namespace Avalux.OpenAi.Client.Tools
                         SyntaxFactory.ParseMemberDeclaration(
                             $"protected {_protocol.Name.Pascalize()}ClientBase(Uri apiUri)\n" +
                             "{\n" +
-                            "    _httpClient = new HttpClient { BaseAddress = apiUri };\n" +
-                            $"    {string.Join("\n", _protocol.Tools.Select(tool => $"AddFunction<{ResolveType(tool.InputType).ToFullString()}, " + $"{ResolveType(tool.OutputType).ToFullString()}>(\"{tool.Name}\", {tool.Name.Pascalize()});"))}\n" +
+                            "    _client = new Avalux.OpenAi.Client.Client(new HttpClient { BaseAddress = apiUri });\n" +
+                            $"    {string.Join("\n", _protocol.Tools.Select(tool => $"_client.AddFunction<{ResolveType(tool.InputType).ToFullString()}, " + $"{ResolveType(tool.OutputType).ToFullString()}>(\"{tool.Name}\", {tool.Name.Pascalize()});"))}\n" +
                             "}") ?? throw new Exception("Internal error"),
                         SyntaxFactory.ParseMemberDeclaration(
                             $"protected {_protocol.Name.Pascalize()}ClientBase(HttpClient httpClient)\n" +
                             "{\n" +
-                            "    _httpClient = httpClient;\n" +
-                            $"    {string.Join("\n", _protocol.Tools.Select(tool => $"AddFunction<{ResolveType(tool.InputType).ToFullString()}, " + $"{ResolveType(tool.OutputType).ToFullString()}>(\"{tool.Name}\", {tool.Name.Pascalize()});"))}\n" +
+                            "    _client = new Avalux.OpenAi.Client.Client(httpClient);\n" +
+                            $"    {string.Join("\n", _protocol.Tools.Select(tool => $"_client.AddFunction<{ResolveType(tool.InputType).ToFullString()}, " + $"{ResolveType(tool.OutputType).ToFullString()}>(\"{tool.Name}\", {tool.Name.Pascalize()});"))}\n" +
                             "}") ?? throw new Exception("Internal error"),
                         SyntaxFactory.FieldDeclaration(
-                                SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("HttpClient"))
+                                SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("Avalux.OpenAi.Client.Client"))
                                     .WithVariables(SyntaxFactory.SeparatedList(new[]
-                                        { SyntaxFactory.VariableDeclarator(SyntaxFactory.Identifier("_httpClient")) })))
+                                        { SyntaxFactory.VariableDeclarator(SyntaxFactory.Identifier("_client")) })))
                             .AddModifiers(
                                 SyntaxFactory.Token(SyntaxKind.PrivateKeyword),
                                 SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword)
@@ -190,80 +192,6 @@ namespace Avalux.OpenAi.Client.Tools
                             )
                             .Cast<MemberDeclarationSyntax>()
                             .ToArray()
-                    )
-                    .AddMembers(
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private class AiMessage\n{\n    [JsonPropertyName(\"role\")] public string? Role { get; init; }\n\n    [JsonPropertyName(\"content\")] public string? Content { get; init; }\n\n    [JsonPropertyName(\"tool_call_id\")] public string? ToolCallId { get; init; }\n\n    [JsonPropertyName(\"tool_calls\")] public ToolCall[]? ToolCalls { get; init; }\n}") ??
-                        throw new Exception("Internal error"),
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private class AiRequestModel\n{\n    [JsonPropertyName(\"messages\")] public AiMessage[] Messages { get; set; } = [];\n}") ??
-                        throw new Exception("Internal error"),
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private class AiResponseModel\n{\n    [JsonPropertyName(\"messages\")] public AiMessage[] Messages { get; set; } = [];\n}") ??
-                        throw new Exception("Internal error"),
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private class FunctionCall\n{\n    [JsonPropertyName(\"name\")] public required string Name { get; init; }\n\n    [JsonPropertyName(\"arguments\")] public required string Arguments { get; init; }\n}") ??
-                        throw new Exception("Internal error"),
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private class ToolCall\n{\n    [JsonPropertyName(\"id\")] public required string Id { get; init; }\n\n    [JsonPropertyName(\"type\")] public string? Type { get; init; }\n\n    [JsonPropertyName(\"function\")] public required FunctionCall Function { get; init; }\n}") ??
-                        throw new Exception("Internal error")
-                    )
-                    .AddMembers(
-                        SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName("int"), "MaxToolCalls")
-                            .AddModifiers(
-                                SyntaxFactory.Token(SyntaxKind.ProtectedKeyword),
-                                SyntaxFactory.Token(SyntaxKind.VirtualKeyword)
-                            ).AddAccessorListAccessors(
-                                SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
-                                    .WithExpressionBody(
-                                        SyntaxFactory.ArrowExpressionClause(SyntaxFactory.ParseExpression("100")))
-                                    .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))
-                            ),
-                        SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName("int"), "MaxRetries")
-                            .AddModifiers(
-                                SyntaxFactory.Token(SyntaxKind.ProtectedKeyword),
-                                SyntaxFactory.Token(SyntaxKind.VirtualKeyword)
-                            ).AddAccessorListAccessors(
-                                SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
-                                    .WithExpressionBody(
-                                        SyntaxFactory.ArrowExpressionClause(SyntaxFactory.ParseExpression("3")))
-                                    .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))
-                            ),
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private record Function(string Name, Func<string, Task<object?>> Func);") ??
-                        throw new Exception("Internal error"),
-                        SyntaxFactory.ParseMemberDeclaration("private readonly List<Function> _functions = [];") ??
-                        throw new Exception("Internal error"),
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private async Task<string?> ProcessAsync<TIn>(string url, TIn request)\n{\n    var resp = await SendInitAsync(url, request);\n    for (int i = 0; i < MaxToolCalls; i++)\n    {\n        var retry = 0;\n        while (true)\n        {\n            try\n            {\n                var messages = resp.Messages.ToList();\n                var lastMessage = messages.Last();\n                if (lastMessage.ToolCalls == null || lastMessage.ToolCalls.Length == 0)\n                {\n                    Console.WriteLine($\"AI Result: {lastMessage.Content}\");\n                    if (lastMessage.Content == null)\n                        throw new Exception(\"Invalid response from AI\");\n                    return lastMessage.Content;\n                }\n\n                resp = await SendAsync(url, new AiRequestModel\n                {\n                    Messages = messages.Concat(await CallToolsAsync(lastMessage)).ToArray()\n                });\n                break;\n            }\n            catch (Exception)\n            {\n                retry++;\n                if (retry > MaxRetries)\n                    throw;\n                Console.WriteLine($\"Retry ({retry}/{MaxRetries})...\");\n            }\n        }\n    }\n\n    throw new Exception(\"Max calls reached\");\n}") ??
-                        throw new Exception("Internal error"),
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private async Task<AiResponseModel> SendAsync(string url, AiRequestModel request)\n{\n    var content = JsonContent.Create(request);\n    var resp = await _httpClient.PostAsync(url + \"/request\", content);\n    resp.EnsureSuccessStatusCode();\n    var result = await resp.Content.ReadFromJsonAsync<AiResponseModel>() ??\n                 throw new Exception(\"Failed to send request\");\n    return result;\n}") ??
-                        throw new Exception("Internal error")
-                        ,
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private async Task<AiResponseModel> SendInitAsync<TIn>(string url, TIn request)\n{\n    var content = JsonContent.Create(request);\n    var resp = await _httpClient.PostAsync(url + \"/init\", content);\n    resp.EnsureSuccessStatusCode();\n    var result = await resp.Content.ReadFromJsonAsync<AiResponseModel>() ??\n                 throw new Exception(\"Failed to send request\");\n    return result;\n}") ??
-                        throw new Exception("Internal error")
-                        ,
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private async Task<List<AiMessage>> CallToolsAsync(AiMessage message)\n    {\n        var messages = new List<AiMessage>();\n        if (message.ToolCalls == null)\n            return messages;\n        foreach (var toolCall in message.ToolCalls)\n        {\n            Console.WriteLine($\"Calling tool {toolCall.Function.Name}({toolCall.Function.Arguments})\");\n            var function = _functions.Single(f => f.Name == toolCall.Function.Name);\n            object? res;\n            try\n            {\n                res = await function.Func(toolCall.Function.Arguments);\n            }\n            catch (Exception e)\n            {\n                Console.WriteLine($\"Error in tool call: {toolCall.Function.Name}: {e.Message}\");\n                res = null;\n            }\n\n            messages.Add(new AiMessage\n            {\n                Role = \"tool\",\n                Content = JsonSerializer.Serialize(res),\n                ToolCallId = toolCall.Id,\n            });\n        }\n\n        return messages;\n    }") ??
-                        throw new Exception("Internal error")
-                        ,
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private void AddFunction<TIn, TOut>(string name, Func<TIn?, Task<TOut>> func)\n{\n    _functions.Add(new Function(name, async data =>\n    {\n        var param = JsonSerializer.Deserialize<TIn>(data);\n        return await func(param);\n    }));\n}") ??
-                        throw new Exception("Internal error")
-                        ,
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private string? ProcessString(string? content)\n{\n    return content;\n}") ??
-                        throw new Exception("Internal error")
-                        ,
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private T? ProcessJson<T>(string? content)\n{\n    if (content == null)\n        return default;\n    if (content.Contains(\"```json\"))\n    {\n        content = content.Substring(\n            content.IndexOf(\"```json\", StringComparison.InvariantCulture) + \"```json\".Length);\n        content = content.Substring(0, content.IndexOf(\"```\", StringComparison.InvariantCulture));\n    }\n\n    return JsonSerializer.Deserialize<T>(content);\n}") ??
-                        throw new Exception("Internal error")
-                        ,
-                        SyntaxFactory.ParseMemberDeclaration(
-                            "private string? ProcessCode(string? content)\n{\n    if (content == null)\n        return null;\n    if (content.Contains(\"```\"))\n    {\n        content = content.Substring(\n            content.IndexOf(\"```\", StringComparison.InvariantCulture) + \"```\".Length);\n        content = content.Substring(0, content.IndexOf(\"```\", StringComparison.InvariantCulture));\n    }\n\n    return content;\n}") ??
-                        throw new Exception("Internal error")
                     );
 
             return classDeclaration;
@@ -272,9 +200,9 @@ namespace Avalux.OpenAi.Client.Tools
         private string GetEndpointExpression(ProtocolEndpoint endpoint)
         {
             var typeParam = endpoint.Mode == ProtocolEndpointMode.Json
-                ? $"<{ResolveType(endpoint.OutputType).ToFullString()}>"
-                : "";
-            return $"Process{endpoint.Mode}{typeParam}(await ProcessAsync(\"{endpoint.Name}\", param))";
+                ? $"<{ResolveType(endpoint.InputType).ToFullString()}, {ResolveType(endpoint.OutputType).ToFullString()}>"
+                : $"<{ResolveType(endpoint.InputType).ToFullString()}>";
+            return $"await _client.Send{endpoint.Mode}RequestAsync{typeParam}(param)";
         }
 
         private ClassDeclarationSyntax GenerateModelClass(ProtocolCustomType customType)
@@ -318,6 +246,17 @@ namespace Avalux.OpenAi.Client.Tools
 
             return SyntaxFactory.AttributeList(
                 SyntaxFactory.SingletonSeparatedList(attribute));
+        }
+
+        public void GeneratePromptFiles(string projectPath)
+        {
+            Directory.CreateDirectory(Path.Combine(projectPath, "Prompts"));
+            foreach (var endpoint in _protocol.Endpoints)
+            {
+                var file = Path.Combine(projectPath, endpoint.Name.Pascalize() + ".prompt.txt");
+                if (!File.Exists(file))
+                    File.WriteAllText(file, string.Empty);
+            }
         }
     }
 }
