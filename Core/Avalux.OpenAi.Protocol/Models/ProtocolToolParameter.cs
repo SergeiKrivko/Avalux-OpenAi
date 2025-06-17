cc9d5f905c1f014fbb519c3566453bb493c26c8c
SergeiKrivko <sergik.krivko@gmail.com>
2025-06-17 22:46:58 +0300

    Генерация Tools.json

---
+++ Core/Avalux.OpenAi.Protocol/Models/ProtocolToolParameter.cs
@@ -0,0 +1,64 @@
+﻿namespace Avalux.OpenAi.Protocol.Models
+{
+    public class ProtocolToolParameter
+    {
+        public string Name { get; set; }
+        public IProtocolType Type { get; set; }
+        public string Description { get; set; }
+        public string Example { get; set; }
+
+        private string GetOpenApiType()
+        {
+            if (Type is ProtocolBuiltInType builtInType)
+            {
+                switch (builtInType.Name)
+                {
+                    case "int":
+                        return "int";
+                    case "float":
+                        return "float";
+                    case "bool":
+                        return "bool";
+                    default:
+                        return "string";
+                }
+            }
+
+            return "object";
+        }
+
+        private string GetOpenApiFormat()
+        {
+            if (Type is ProtocolBuiltInType builtInType)
+            {
+                switch (builtInType.Name)
+                {
+                    case "uuid":
+                        return "uuid";
+                    case "datetime":
+                        return "datetime";
+                    case "date":
+                        return "date";
+                    case "time":
+                        return "time";
+                    default:
+                        return "";
+                }
+            }
+
+            return "";
+        }
+
+        public ApiToolParameter ToApiParameter()
+        {
+            return new ApiToolParameter
+            {
+                Name = Name,
+                Description = Description,
+                Type = GetOpenApiType(),
+                Required = !(Type is ProtocolNullableType),
+                Format = GetOpenApiFormat(),
+            };
+        }
+    }
+}
--- Core/Avalux.OpenAi.Protocol/ProtocolParser.cs
+++ Core/Avalux.OpenAi.Protocol/ProtocolParser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Avalux.OpenAi.Protocol.Models;
 using Avalux.OpenAi.Protocol.Schemas;
 using YamlDotNet.Serialization;
@@ -8,90 +10,97 @@ using YamlDotNet.Serialization.NamingConventions;
 namespace Avalux.OpenAi.Protocol
 {
     public class ProtocolParser
-{
-    private readonly IDeserializer _deserializer;
-
-    public ProtocolParser()
     {
-        _deserializer = new DeserializerBuilder()
-            .WithNamingConvention(UnderscoredNamingConvention.Instance)
-            .Build();
-    }
+        private readonly IDeserializer _deserializer;
 
-    private Models.Protocol Parse(string data, string defaultName)
-    {
-        var apiSchema = _deserializer.Deserialize<ApiSchema>(data);
-        var protocol = new Models.Protocol { Name = apiSchema.Name ?? defaultName };
-        foreach (var item in apiSchema.Schemas)
+        public ProtocolParser()
         {
-            protocol.CustomTypes[item.Key] = new ProtocolCustomType(item.Key, item.Value);
+            _deserializer = new DeserializerBuilder()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                .Build();
         }
 
-        foreach (var protocolCustomType in protocol.CustomTypes.Values)
+        private Models.Protocol Parse(string data, string defaultName)
         {
-            protocolCustomType.ResolveFields(protocol);
+            var apiSchema = _deserializer.Deserialize<ApiSchema>(data);
+            var protocol = new Models.Protocol { Name = apiSchema.Name ?? defaultName };
+            foreach (var item in apiSchema.Schemas)
+            {
+                protocol.CustomTypes[item.Key] = new ProtocolCustomType(item.Key, item.Value);
+            }
+
+            foreach (var protocolCustomType in protocol.CustomTypes.Values)
+            {
+                protocolCustomType.ResolveFields(protocol);
+            }
+
+            foreach (var item in apiSchema.Tools)
+            {
+                protocol.Tools.Add(ParseTool(item.Key, item.Value, protocol));
+            }
+
+            foreach (var item in apiSchema.Endpoints)
+            {
+                protocol.Endpoints.Add(ParseEndpoint(item.Key, item.Value, protocol));
+            }
+
+            return protocol;
         }
 
-        foreach (var item in apiSchema.Tools)
+        public Models.Protocol ParseFile(string path)
         {
-            protocol.Tools.Add(ParseTool(item.Key, item.Value, protocol));
+            return Parse(File.ReadAllText(path), Path.GetFileNameWithoutExtension(path));
         }
 
-        foreach (var item in apiSchema.Endpoints)
+        private static ProtocolTool ParseTool(string name, ApiToolSchema toolSchema, Models.Protocol protocol)
         {
-            protocol.Endpoints.Add(ParseEndpoint(item.Key, item.Value, protocol));
+            return new ProtocolTool
+            {
+                Name = name,
+                Description = toolSchema.Description,
+                Parameters = toolSchema.Params.Select(p => new ProtocolToolParameter
+                {
+                    Name = p.Name,
+                    Description = p.Description,
+                    Type = protocol.ResolveType(p.Type),
+                    Example = p.Example,
+                }).ToArray(),
+                ResultType = protocol.ResolveType(toolSchema.Result)
+            };
         }
 
-        return protocol;
-    }
-
-    public Models.Protocol ParseFile(string path)
-    {
-        return Parse(File.ReadAllText(path), Path.GetFileNameWithoutExtension(path));
-    }
-
-    private static ProtocolTool ParseTool(string name, ApiToolSchema toolSchema, Models.Protocol protocol)
-    {
-        return new ProtocolTool
+        private static ProtocolEndpoint ParseEndpoint(string name, ApiEndpointSchema endpointSchema,
+            Models.Protocol protocol)
         {
-            Name = name,
-            Description = toolSchema.Description,
-            InputType = protocol.ResolveType(toolSchema.Input),
-            OutputType = protocol.ResolveType(toolSchema.Output)
-        };
-    }
+            ProtocolEndpointMode mode;
+            switch (endpointSchema.Mode)
+            {
+                case "text":
+                    mode = ProtocolEndpointMode.Text;
+                    break;
+                case "json":
+                    mode = ProtocolEndpointMode.Json;
+                    break;
+                case "code":
+                    mode = ProtocolEndpointMode.Code;
+                    break;
+                default:
+                    throw new Exception($"Unsupported endpoint mode: {endpointSchema.Mode}");
+            }
 
-    private static ProtocolEndpoint ParseEndpoint(string name, ApiEndpointSchema endpointSchema,
-        Models.Protocol protocol)
-    {
-        ProtocolEndpointMode mode;
-        switch (endpointSchema.Mode)
-        {
-            case "text":
-                mode = ProtocolEndpointMode.Text;
-                break;
-            case "json":
-                mode = ProtocolEndpointMode.Json;
-                break;
-            case "code":
-                mode = ProtocolEndpointMode.Code;
-                break;
-            default:
-                throw new Exception($"Unsupported endpoint mode: {endpointSchema.Mode}");
+            var result = new ProtocolEndpoint
+            {
+                Name = name,
+                InputType = protocol.ResolveType(endpointSchema.Input),
+                OutputType = protocol.ResolveType(endpointSchema.Output),
+                Mode = mode
+            };
+            if (result.Mode == ProtocolEndpointMode.Text && !result.OutputType.IsString)
+                throw new Exception("'string' mode requires a 'string' output type");
+            if (result.Mode == ProtocolEndpointMode.Code && !result.OutputType.IsString)
+                throw new Exception("'code' mode requires a 'string' output type");
+
+            return result;
         }
-        var result = new ProtocolEndpoint
-        {
-            Name = name,
-            InputType = protocol.ResolveType(endpointSchema.Input),
-            OutputType = protocol.ResolveType(endpointSchema.Output),
-            Mode = mode
-        };
-        if (result.Mode == ProtocolEndpointMode.Text && !result.OutputType.IsString)
-            throw new Exception("'string' mode requires a 'string' output type");
-        if (result.Mode == ProtocolEndpointMode.Code && !result.OutputType.IsString)
-            throw new Exception("'code' mode requires a 'string' output type");
-
-        return result;
     }
-}
 }
